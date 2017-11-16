7f53e627094e1a5d77a0edaa60f3562e0bba3168
Ben Parman <benparman@gmail.com>
2017-11-15 22:09:30 -0700

    Working question database in app.js

---
+++ app.js
@@ -0,0 +1,77 @@
+'use strict';
+//*******************
+//*******************
+//STORE
+//*******************
+//*******************
+
+//*******************
+//*******************
+//Template Generators
+//*******************
+//*******************
+
+//*******************
+//*******************
+//Question Database
+//*******************
+//*******************
+const questionDatabase = 
+[
+  {
+    question: 'There has been a recent trend shift in mountain bike geometry in recent years (now being 2017).  Which option best describes this shift?',
+    correctAnswer: 'Long, Slack, and Low',
+    incorrectAnswers: [
+      'Short, Tall, and Steep',
+      'Obtuse, parallel, and perpendicular',
+      'Triangular, curvy, and straight',
+      'Long chainstays, short top tubes, and vertical head tubes'
+    ]
+  },
+  {
+    question: 'What standard in rear wheel dropouts replaced the 135mm quick release?',
+    correctAnswer: '142mm Thru-Axle',
+    incorrectAnswers: [
+      '200mm Traxle -Bolt',
+      'The english threaded reverse bolt',
+      'Boost 148',
+      'BSA Tapered Spindle'
+    ]
+  },
+  {
+    question: 'What suspension feature should be adjusted to prevent unwanted ‘dive’ under braking?',
+    correctAnswer: 'Low-speed compression damping',
+    incorrectAnswers: [
+      'Rebound Damping',
+      'Negative air spring pressure',
+      'Lockout force threshold',
+      'Top-out bumper return spring preload'
+    ]
+  },
+  {
+    question: 'Coil springs have been largely replaced by air springs in modern bicycle suspension systems.  In which riding discipline will you still find prevalent usage of coil springs?',
+    correctAnswer: 'Downhill & Enduro',
+    incorrectAnswers: [
+      'World-cup cross country racing',
+      'Ultra-Endurance',
+      'Short Track',
+      'Dirt Jumping & BMX'
+    ]
+  },
+  {
+    question: 'The trend in 1x11 drivetrains was led by SRAM and their development of the XD freehub body.  The XD freehub body provided what capability over a standard splined freehub body?',
+    correctAnswer: 'The ability to utilize a 10 tooth cassette cog',
+    incorrectAnswers: [
+      'Free-floating midrange spider compatibility',
+      'Reverse-cut outboard carrier bearing housing',
+      'One-piece cassette compatibility',
+      'Nothing, it was simply a ploy to monopolize the drivetrain market by using proprietary technology'
+    ]
+  }
+];
+
+//*******************
+//*******************
+//HTML Renderer
+//*******************
+//*******************
